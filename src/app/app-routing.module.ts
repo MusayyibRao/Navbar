5aa1487702c965dfa2c74969db5e90dae7d48689
MusayyibRao <musarao6396@gmail.com>
2021-11-05 00:48:26 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component:RegisterPageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'userlist',component:UserlistComponent},
+  {path:'home',component:HomeComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginComponent,
+    UserlistComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxPaginationModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
